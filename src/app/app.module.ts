82e82da2cc737f89c44be8dee38a540cf3d106aa
Daniels <thaddeus.daniels@accenture.com>
2023-08-08 10:40:14 -0500

    adds top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
