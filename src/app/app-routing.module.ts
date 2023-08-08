602f5072130034ede6cb25029049361f1c51aca0
Daniels <thaddeus.daniels@accenture.com>
2023-08-08 13:33:05 -0500

    adds paths for routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { WeatherDetailComponent } from './dashboard/weather-detail/weather-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'locations', component: DashboardComponent },
+  { path: 'locations/:id', component: WeatherDetailComponent },
+  { path: '', redirectTo: 'locations', pathMatch: 'full' },
+  { path: '**', redirectTo: 'locations', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
