73692d46b8d42b80f6f00987cb8c2125c71861db
Daniels <thaddeus.daniels@accenture.com>
2023-08-08 14:53:39 -0500

    routing experimentation

--- src/app/dashboard/weather-detail/weather-detail.component.ts
+++ src/app/dashboard/weather-detail/weather-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from 'src/app/sample-locations';
 
 @Component({
   selector: 'mwa-weather-detail',
   templateUrl: './weather-detail.component.html',
   styleUrls: ['./weather-detail.component.css']
 })
-export class WeatherDetailComponent {
+export class WeatherDetailComponent implements OnInit{
+  pageTitle: string = 'Weather detail';
+  location: Location | undefined;
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
+    
+    this.location = {
+      'city': 'Chicago',
+      'currentTemp': 78,
+      'conditions': 'Sunny',
+      'id': id
+    }
+    this.pageTitle += `: ${this.location.city}`
   }
 }
--- src/app/sample-locations.ts
+++ src/app/sample-locations.ts
@@ -1,4 +1,5 @@
 export interface Location {
+    id: number,
     city: string,
     currentTemp: number,
     conditions: string
